3e56caadd938b0ef63f920f5b15e0be77a190b5b
Romeu Beato <romeufbeato@gmail.com>
2021-03-19 11:01:43 +0000

    first commit

---
+++ test/unit/TestUserStorage.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.4;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../../contracts/users/UserStorage.sol";
