32a2a2b36c1eb121df117441e61956222a066c8d
Romeu Beato <romeufbeato@gmail.com>
2021-03-29 16:35:51 +0100

    restarting

--- test/unit/TestUserStorage.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.7.4;
-
-import "truffle/Assert.sol";
-import "truffle/DeployedAddresses.sol";
-import "../../contracts/users/UserStorage.sol";
